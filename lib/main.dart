422563e973cfe3fbfc645475e99350fb027ff30a
황교욱 <hkw0101@gmail.com>
2021-12-22 22:23:23 +0900

    ##카드뷰 LayoutBuilder 이용해보기 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_story_app_ui/widgets/cardscroollwidget.dart';
 import 'customicons.dart';
 import 'data.dart';
 import 'dart:math';
@@ -75,10 +76,10 @@ class _MyAppState extends State<MyApp> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "Trending",
+                      "트렌디",
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 46.0,
+                        fontSize: 40.0,
                         fontFamily: 'Calibre-Semibold',
                         letterSpacing: 1.0,
                       ),
@@ -93,6 +94,9 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ],
                 ),
+              ),
+              SizedBox(
+                height: 12,
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 20.0),
@@ -146,7 +150,7 @@ class _MyAppState extends State<MyApp> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "Favorite",
+                      "즐겨찾기",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 40.0,
@@ -161,6 +165,9 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ],
                 ),
+              ),
+              SizedBox(
+                height: 12,
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 20.0),
@@ -212,124 +219,6 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class CardScrollWidget extends StatelessWidget {
-  var currentPage;
-  var padding = 20.0;
-  var verticalInset = 20.0;
-
-  CardScrollWidget(this.currentPage);
-
-  @override
-  Widget build(BuildContext context) {
-    return new AspectRatio(
-      aspectRatio: widgetAspectRatio,
-      child: LayoutBuilder(builder: (context, contraints) {
-        //LayoutBuilder 위젯의 크기
-        //LayoutBuilder의 builder function은 layout타임에 불리게 되고, constraints이 제공됩니다.
-        // 처음으로 widget이 layout 될 때
-        // 부모 widget의 constraint가 바뀔 때
-        // 부모 widget이 해당 widget을 업데이트할 때
-        var width = contraints.maxWidth;
-        var height = contraints.maxHeight;
-
-        var safeWidth = width - 2 * padding;
-        var safeHeight = height - 2 * padding;
-
-        var heightOfPrimaryCard = safeHeight;
-        var widthOfPrimaryCard = heightOfPrimaryCard * (cardAspectRatio);
-
-        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
-        var horizontalInset = primaryCardLeft / 2;
-
-        List<Widget> cardList = [];
-
-        for (var i = 0; i < images.length; i++) {
-          //current 현재페이지 순서 이미지길이(개수)
-          var delta = i - (currentPage);
-          bool isOnRight = delta > 0;
-
-          var start = padding +
-              max(
-                  primaryCardLeft -
-                      horizontalInset * -delta * (isOnRight ? 15 : 1),
-                  0.0);
-          var cardItem = Positioned.directional(
-            top: padding + verticalInset * max(-delta, 0.0),
-            bottom: padding + verticalInset * max(-delta, 0.0),
-            start: start,
-            textDirection: TextDirection.rtl,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16.0),
-              child: Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(3.0, 6.0),
-                      blurRadius: 10.0)
-                ]),
-                child: AspectRatio(
-                  aspectRatio: cardAspectRatio,
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: <Widget>[
-                      Image.asset(
-                        images[i],
-                        fit: BoxFit.cover,
-                      ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 16.0, vertical: 8.0),
-                              child: Text(
-                                title[i],
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 25.0,
-                                    fontFamily: 'SF-Pro-Text-Regular'),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(left: 12, bottom: 12),
-                              child: Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 22.0, vertical: 6.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.blueAccent,
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                                child: Text("Read Later",
-                                    style: TextStyle(color: Colors.white)),
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          );
-          cardList.add(cardItem);
-        }
-
-        return Stack(
-          children: cardList,
-        );
-      }),
     );
   }
 }
--- lib/widgets/cardscroollwidget.dart
+++ lib/widgets/cardscroollwidget.dart
@@ -4,3 +4,121 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_story_app_ui/data.dart';
 import 'package:flutter_story_app_ui/main.dart';
+
+class CardScrollWidget extends StatelessWidget {
+  var currentPage;
+  var padding = 20.0;
+  var verticalInset = 20.0;
+
+  CardScrollWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(builder: (context, contraints) {
+        //LayoutBuilder 위젯의 크기
+        //LayoutBuilder의 builder function은 layout타임에 불리게 되고, constraints이 제공됩니다.
+        // 처음으로 widget이 layout 될 때
+        // 부모 widget의 constraint가 바뀔 때
+        // 부모 widget이 해당 widget을 업데이트할 때
+        var width = contraints.maxWidth;
+        var height = contraints.maxHeight;
+
+        var safeWidth = width - 2 * padding;
+        var safeHeight = height - 2 * padding;
+
+        var heightOfPrimaryCard = safeHeight;
+        var widthOfPrimaryCard = heightOfPrimaryCard * (cardAspectRatio);
+
+        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+        var horizontalInset = primaryCardLeft / 2;
+
+        List<Widget> cardList = [];
+
+        for (var i = 0; i < images.length; i++) {
+          //current 현재페이지 순서 이미지길이(개수)
+          var delta = i - (currentPage);
+          bool isOnRight = delta > 0;
+
+          var start = padding +
+              max(
+                  primaryCardLeft -
+                      horizontalInset * -delta * (isOnRight ? 15 : 1),
+                  0.0);
+          var cardItem = Positioned.directional(
+            top: padding + verticalInset * max(-delta, 0.0),
+            bottom: padding + verticalInset * max(-delta, 0.0),
+            start: start,
+            textDirection: TextDirection.rtl,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(3.0, 6.0),
+                      blurRadius: 10.0)
+                ]),
+                child: AspectRatio(
+                  aspectRatio: cardAspectRatio,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Image.asset(
+                        images[i],
+                        fit: BoxFit.cover,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 16.0, vertical: 8.0),
+                              child: Text(
+                                title[i],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25.0,
+                                    fontFamily: 'SF-Pro-Text-Regular'),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 12, bottom: 12),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 22.0, vertical: 6.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.blueAccent,
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                child: Text("Read Later",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+          cardList.add(cardItem);
+        }
+
+        return Stack(
+          children: cardList,
+        );
+      }),
+    );
+  }
+}
