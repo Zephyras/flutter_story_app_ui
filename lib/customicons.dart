ee706d9e26e92ccb380237a11ef6d08a727e58f4
황교욱 <hkw0101@gmail.com>
2021-12-22 21:55:01 +0900

    ##카드뷰 LayoutBuilder 이용해보기 완료

---
+++ lib/customicons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//커스텀 아이콘 만들기
+//디자인너한테 받은 아이콘시트로 아이콘정리할때 요렇게 정리하면 깔끔할듯함.
+class CustomIcons {
+  static const IconData menu = IconData(0xe900, fontFamily: 'CustomIcons');
+  static const IconData option = IconData(0xe902, fontFamily: 'CustomIcons');
+}
---
+++ lib/widgets/cardscroollwidget.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_story_app_ui/data.dart';
+import 'package:flutter_story_app_ui/main.dart';
